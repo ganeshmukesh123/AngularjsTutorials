61d79e2425e912c5bedb417d0adf47546c02c4e9
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-12-29 01:40:04 +0530

    adding hero detail component and able to display hero list and their details onclick

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';// <-- NgModel lives here
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule //<-- import the FormsModule before binding with [(ngModel)]
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
